62d2df1d84da7354ba1dbbf8dcfcbf6ddaf98e11
SKURIC Antun <antun.skuric@inria.fr>
2021-10-25 08:38:55 +0200

    v1.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pycapacity',
-    version='1.0.4',
+    version='1.0.5',
     author='Antun Skuric',
     author_email='antun.skuric@inria.fr',
     description='A Real-time capable robot capacity calculation module',
