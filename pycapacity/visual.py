187a4344fb6b5e7085b320dae29266f389e8711e
SKURIC Antun <antun.skuric@inria.fr>
2021-12-07 11:48:23 +0100

    added example

--- pycapacity/visual.py
+++ pycapacity/visual.py
@@ -6,10 +6,15 @@ def plot_polytope_faces(faces,ax=None, plt=None, face_color=None, edge_color=Non
     """
     Polytope faces plotting function in 2d and 3d. 
     
-    Notes
-    -----
-    If provided matplotlib plt it will find the ax automatically. No need to provide the ax. If ax already defined no need to provide the plt.
+    Note:
+        If provided matplotlib `plt` it will find the `ax` automatically. No need to provide the `ax`. If `ax` already defined no need to provide the `plt`.
 
+    Examples:
+        >> import pycapacity.visual
+        >> import matplotlib.pyplot as plt
+        >> ax = pycapacity.visual.plot_polytope_faces([[6,5,4,3],[1,2,2,1]],plt=plt,face_color='blue')
+        >> pycapacity.visual.plot_polytope_faces([[10,8,7,6],[1,2,2,1]],ax=ax,face_color='red')
+        >> plt.show()
 
     Args:
         faces:  list of faces (vertices)
