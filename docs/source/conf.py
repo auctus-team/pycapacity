8b1b43b6319d58193ac23401d0b89ad2f0c23a78
askuric <you@example.com>
2023-04-17 10:57:51 +0200

    jquiry added

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,6 +45,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'recommonmark',
+    "sphinxcontrib.jquery",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
